3e6a2b58cd04da703e2ae35731f2c6003e45d5bd
Giovane <giovaneaalmeida27@gmail.com>
2023-02-01 19:17:28 -0300

    feat: create publish function

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -1,6 +1,10 @@
 package rabbitmq
 
-import amqp "github.com/rabbitmq/amqp091-go"
+import (
+	"context"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func OpenChannel() *amqp.Channel {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
@@ -31,6 +35,17 @@ func Consume(ch *amqp.Channel, out chan<- amqp.Delivery) error {
 	}
 	for msg := range msgs {
 		out <- msg
+	}
+	return nil
+}
+
+func Publish(ch *amqp.Channel, body string, ctx context.Context) error {
+	err := ch.PublishWithContext(ctx, "amq.direct", "", false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(body),
+	})
+	if err != nil {
+		return err
 	}
 	return nil
 }
